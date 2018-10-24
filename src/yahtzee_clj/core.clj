228d73947f2f29706dd45883d0231c733e20ea5d
= <=>
2018-10-24 08:57:28 +0100

    Initial commit

---
+++ src/yahtzee_clj/core.clj
@@ -0,0 +1,15 @@
+(ns yahtzee-clj.core
+  (:gen-class))
+
+(defn roll []
+  "Rolls a six sided die once."
+  (+ 1 (rand-int 6)))
+
+(defn roll-hand []
+  "Rolls a hand of 5 six sided dice."
+  (repeatedly 5 roll))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (roll)))
